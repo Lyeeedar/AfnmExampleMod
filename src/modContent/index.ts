c4e1c3da7001a43dd4d2716aa678fa19d0cdaf33
Philip Liddell <lyeeedar@gmail.com>
2026-05-12 14:06:59 +0100

    add one hp mod

--- src/modContent/index.ts
+++ src/modContent/index.ts
@@ -1,31 +1,8 @@
 import { TreasureItem, Technique } from 'afnm-types';
 
-import icon from '../assets/image.png';
+window.modAPI.hooks.onCreatePlayerCombatEntity((player, combatEntity, breakthrough, flags) => {
+  combatEntity.stats.maxhp = 1
+  combatEntity.stats.hp = 1
 
-const myTreasure: TreasureItem = {
-  kind: 'treasure',
-  name: 'The Best Treasure',
-  description: 'Wooo mod content.',
-  icon: icon,
-  stacks: 1,
-  rarity: 'mundane',
-  realm: 'coreFormation',
-};
-window.modAPI.actions.addItem(myTreasure);
-
-const myTechnique: Technique = {
-  name: 'Test technique',
-  icon: '',
-  type: 'fist',
-  effects: [
-    {
-      kind: 'buffSelf',
-      buff: window.modAPI.gameData.techniqueBuffs.fist.flow,
-      amount: {
-        value: 1,
-        stat: undefined,
-      },
-    },
-  ],
-};
-window.modAPI.actions.addTechnique(myTechnique);
+  return combatEntity
+})
